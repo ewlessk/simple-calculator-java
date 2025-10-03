3dd7414de7793d662aee8ffafe6338feed5a3156
elriww <elriwwyeah@gmail.com>
2025-10-03 12:36:50 +0300

    feat: добавлен метод multiply

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 + 3 = " + add(2, 3));
+        System.out.println("5 - 2 = " + subtract(5, 2));
+    }
+    public static int multiply(int a, int b) {
+    return a * b;
+}
+
+}
